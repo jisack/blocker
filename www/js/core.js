117b1fae0b169337fd409b8e0882b409d94c1180
Witsaraz <witsaraz@wisdomlanna.com>
2016-08-16 23:44:21 +0700

    update resize

--- www/js/core.js
+++ www/js/core.js
@@ -1,17 +1,29 @@
 var game = new Phaser.Game('100%', '100%', Phaser.CANVAS, 'RPG', { preload: preload, create: create, update: update, render: render});
 
 function preload(){
+    //textures
     game.load.image('bg', 'assets/bg.svg');
     game.load.image('shadow', 'assets/shadow.svg');
     game.load.image('rock', 'assets/rock.svg');
     game.load.image('tree', 'assets/tree.svg');
     
+    //ui
+    game.load.image('move', 'assets/ui/move.svg');
+    game.load.image('attack', 'assets/ui/attack.svg');
+
+    //fx
     game.load.image('flake', 'assets/fx/flake.svg');
     
+    //creatures
     game.load.spritesheet('zombie', 'assets/zombie.svg',46,46);
     game.load.image('hands', 'assets/weapons/hands.svg');
     game.load.spritesheet('hero', 'assets/hero.svg',46,46);
     game.load.spritesheet('sword', 'assets/weapons/sword.svg',160,160);
+
+    game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+    game.scale.setResizeCallback(function() {
+        game.scale.setMaximum();
+    });
 }
 
 function create(){
@@ -30,27 +42,48 @@ function create(){
     field = game.add.tileSprite(0,0,2000,2000,'bg');
 }
 
+function playerRotation(){
+    var rad = pointToRadian(
+        player.position.x-game.camera.x,
+        player.position.y-game.camera.y,
+        game.input.x,
+        game.input.y);
+    return rad;
+}
+function controllerRotation(){
+    var rad = pointToRadian(
+        60,
+        window.innerHeight-60,
+        game.input.x,
+        game.input.y);
+    return (game.input.x<window.innerWidth/2 && 
+        game.input.y>window.innerHeight/2? rad:null);
+}
+
+function updateUI(){
+    button.move.x = game.camera.x+20;
+    button.move.y = game.camera.y+window.innerHeight-100;
+    button.attack.x = game.camera.x+window.innerWidth-100;
+    button.attack.y = game.camera.y+window.innerHeight-100;
+}
+
 function update(){
     if(map.ready){
+        updateUI();
+
         if (game.input.activePointer.leftButton.isDown || game.input.pointer1.isDown){
-            var rad = pointToRadian(
-                player.position.x-game.camera.x,
-                player.position.y-game.camera.y,
-                game.input.x,
-                game.input.y);
-            player.rotation = rad;
-            ws.send(JSON.stringify({
-                status: 'move',
-                id: player.id,
-                rotation: rad
-            }));
+            var rad = controllerRotation();
+            if(rad){
+                player.rotation = rad;
+                ws.send(JSON.stringify({
+                    status: 'move',
+                    id: player.id,
+                    rotation: rad
+                }));
+            }
         }
-        if (game.input.activePointer.rightButton.isDown || game.input.pointer2.isDown){
-            var rad = pointToRadian(
-                player.position.x-game.camera.x,
-                player.position.y-game.camera.y,
-                game.input.x,
-                game.input.y);
+        if (game.input.activePointer.rightButton.isDown){
+            var rad = playerRotation();
             player.rotation = rad;
             ws.send(JSON.stringify({
                 status: 'attack',
@@ -73,6 +106,8 @@ function render(){
 //Initial
 var field,player,shadows,shadows2,zombies,heroes,trees,rocks,weapons;
 var playId,creatures = {};
+var button = {};
+
 function init(){
     weapons = game.add.group();
     shadows = game.add.group();
@@ -90,6 +125,22 @@ function init(){
 
     playId = localStorage.getItem('uuid')||uuid();
     localStorage.setItem('uuid',playId);
+    ui();
+}
+
+//ui
+function ui(){
+    button.move = game.add.sprite(game.camera.x, game.camera.y+window.innerHeight-256, 'move');
+    button.attack = game.add.button(game.camera.x+window.innerWidth-256, game.camera.y+window.innerHeight-256, 'attack', function(){
+        ws.send(JSON.stringify({
+            status: 'attack',
+            id: player.id,
+            rotation: player.rotation
+        }));
+    }, this, 2, 1, 0);
+
+    button.move.scale.setTo(2, 2);
+    button.attack.scale.setTo(2, 2);
 }
 
 //update map
